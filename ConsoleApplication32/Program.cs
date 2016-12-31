2d7bcbe7d50a2795c2e50b092e609d313cb0030e
fillu87gyc <wander.off65@gmail.com>
2017-01-01 01:49:46 +0900

    完成

--- ConsoleApplication32/Program.cs
+++ ConsoleApplication32/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication32
+class Program
 {
-	class Program
+	static void Main()
 	{
-		static void Main(string[] args)
+		var numb = new int[8];
+		int[] denom = { 2, 0, 4, 4, 0, 8 };
+		for (int i = 2; i < 10; i++)
 		{
+			numb[i - 2] = (int)Math.Pow(2.0, i);
 		}
+		try
+		{
+			for (int i = 0; i < numb.Length; i++)
+			{
+				try
+				{
+					Console.WriteLine(numb[i] + " / " + denom[i] + " is\t" + numb[i] / denom[i]);
+				}
+				catch (DivideByZeroException)
+				{
+					Console.WriteLine("div by zero");
+				}
+			}
+		}
+		catch (IndexOutOfRangeException)
+		{
+			Console.WriteLine("Index out range");
+		}
+
 	}
 }
